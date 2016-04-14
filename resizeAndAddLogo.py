2b66b3df238bac9247e4b60ddd0625e171ec684e
Matthew Marchetti <mmarchetti2@mail.valenciacollege.edu>
2016-04-13 22:00:37 -0400

    Should have worked?
    I'm basically uploading this because I'm not quite sure what to do next.
    I thought it would have worked, but I guess not.

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -8,7 +8,27 @@ logoIm = Image.open( LOGO_FILENAME )
 logoWidth, logoHeight = logoIm.size
 
 #TODO: Loop over all files in the working directory
-#TODO: Check if file image needs to be
+#TODO: Check if file image needs to be resized.
 #TODO: Calculate the new width and height to resize to.
 #TODO: Add the logo.
 #TODO: Save changes.
+
+os.makedirs('withLogo', exist_ok=True)
+for filename in os.listdir('.'):
+    if not (filename.endswith('.png') or filename.endswith('.jpg')) \
+       or filename == LOGO_FILENAME:
+        continue
+    im = Image.open(filename)
+    width, height = im.size
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+            width = int((SQUARE_FIT_SIZE / height) * width)
+            height = SQUARE_FIT_SIZE
+    print('Resizing %s...' % (filename))
+    im = im.resize((width, height))
+    print('Adding logo to %s...' % (filename))
+    im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+    im.save(os.path.join('withLogo', filename))
